9bf6e10b2492ed3cf818bce9661e2d2b9e5475bf
Tanner Patterson <tpatterson5492@gmail.com>
2019-10-04 01:14:54 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
-# Your Code Here
+def map(array)
+new = []
+i = 0
+while i < array.length
+new.push(yield(array[i]))
+i += 1
+end
+new
+end
+
+
+def reduce(array, sp=nil)
+  if sp
+    total = sp
+    i = 0 
+  else 
+    total = array[0]
+    i = 1
+end
+  while i < array.length 
+  total = yield(total, array[i])
+  i += 1
+end
+total
+end
